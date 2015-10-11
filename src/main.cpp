0a019422c18e3275d5b566034c40801894541a66
Craig Lovell <cxgslegend@gmail.com>
2015-10-10 22:23:04 -0400

    Fixed detection of the correct number of arguments passed into main. Also, added code to exit if the input file given is invalid

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,17 @@
 #include "parser.h"
 #include "fileWriter.h"
 #include <vector>
+#include <iostream>
+#include <fstream>
 
 int main(int argc, char* argv[]) {
-  if (argc != 2) {
-
+  if (argc == 3) {
+    std::ifstream inputFile(argv[1]);
+    if(!inputFile.good()) {
+      std::cout << "Invalid input file" << std::endl;
+      std::cout << "Exiting..." << std::endl;
+      return -1;
+    }
     std::vector<std::string> file = readFile(argv[1]);
     std::vector<std::vector<std::string> > parsedFile = parseFile(file);
     assembler assembler;
@@ -17,9 +24,12 @@ int main(int argc, char* argv[]) {
     std::vector<std::string> hexCodes = assembler.getHexCodes();
 
     writeToFile(hexCodes, argv[2]);
+    return 0;
   }
   else {
     std::cout << "Incorrect number of arguments" << std::endl;
+    std::cout << "Exiting..." << std::endl;
+    return -1;
   }
 }
 
